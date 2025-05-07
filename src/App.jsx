cb4054c036642e36d45576a73bbf4398aa71d44d
Abel Contreras <lucas_65_@hotmail.es>
2025-05-07 17:50:38 +0200

    Información del Curso, paso 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-const Header = ({ course }) => {
-  return <h1>{course}</h1>;
+const Header = (props) => {
+  console.log(props);
+  return <h1>{props.course}</h1>;
 };
 
 const Content = ({ part, exercises }) => {
@@ -13,19 +14,30 @@ const Content = ({ part, exercises }) => {
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const part1 = {
+    name: "Fundamentals of React",
+    exercises: 10,
+  };
+  const part2 = {
+    name: "Using props to pass data",
+    exercises: 7,
+  };
+  const part3 = {
+    name: "State of a component",
+    exercises: 14,
+  };
   return (
     <>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <p>
+        {part1.name} {part1.exercises}
+      </p>
+      <p>
+        {part2.name} {part2.exercises}
+      </p>
+      <p>
+        {part3.name} {part3.exercises}
+      </p>
     </>
   );
 };
