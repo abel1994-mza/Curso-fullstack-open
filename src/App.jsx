034067ef7c2188526a95a389df2330ea20c97b9f
Abel Contreras <lucas_65_@hotmail.es>
2025-05-08 11:40:55 +0200

    Ejercicio 1.12*: anecdotes, paso 1 (Boton para pasar a siguiente anecdote)

--- src/App.jsx
+++ src/App.jsx
@@ -39,9 +39,20 @@ const Statistics = ({ good, neutral, bad }) => {
 };
 
 const App = () => {
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [selected, setSelected] = useState(3);
 
   const handleGoodClick = () => {
     setGood(good + 1);
@@ -53,6 +64,11 @@ const App = () => {
     setBad(bad + 1);
   };
 
+  const handleAnecdoteClick = () => {
+    const randomIndex = Math.floor(Math.random() * anecdotes.length);
+    setSelected(randomIndex);
+  };
+
   return (
     <>
       <h1>Give feedback</h1>
@@ -60,8 +76,11 @@ const App = () => {
       <button onClick={handleNeutralClick}>Neutral</button>
       <button onClick={handleBadClick}>Bad</button>
       <h1>Statistics</h1>
-
       <Statistics good={good} neutral={neutral} bad={bad} />
+      <hr />
+      <h1>Anecdote of the day</h1>
+      <p>{anecdotes[selected]}</p>
+      <Button text="Next anecdote" handleClick={handleAnecdoteClick} />
     </>
   );
 };
