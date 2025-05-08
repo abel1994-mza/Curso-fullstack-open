a6319b05e69537a0091c8b71ac7558d812b17544
Abel Contreras <lucas_65_@hotmail.es>
2025-05-08 12:32:18 +0200

    Ejercicio 1.14*: anecdotes, paso 3 (anecdota mas votada)

--- src/App.jsx
+++ src/App.jsx
@@ -91,6 +91,7 @@ const App = () => {
       <h1>Anecdote with most votes</h1>
       <p>{anecdotes[votes.indexOf(Math.max(...votes))]}</p>
       <p>has {Math.max(...votes)} votes</p>
+      <h1>Votes</h1>
     </>
   );
 };
