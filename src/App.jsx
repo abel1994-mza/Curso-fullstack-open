f793680cc4c7081744a161a88ae97df9150e2f89
Abel Contreras <lucas_65_@hotmail.es>
2025-05-08 11:04:46 +0200

    Ejercicio 1.10: unicafe paso 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,48 @@
 import { useState } from "react";
 
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad;
+  const average = (good - bad) / total;
+  const positivePercentage = (good / total) * 100;
+
+  if (total === 0) {
+    return <p>No feedback given</p>;
+  }
+
+  return (
+    <div>
+      <table>
+        <tbody>
+          <tr>
+            <td>Good</td>
+            <td>{good}</td>
+          </tr>
+          <tr>
+            <td>Neutral</td>
+            <td>{neutral}</td>
+          </tr>
+          <tr>
+            <td>Bad</td>
+            <td>{bad}</td>
+          </tr>
+          <tr>
+            <td>Total</td>
+            <td>{total}</td>
+          </tr>
+          <tr>
+            <td>Average</td>
+            <td>{average}</td>
+          </tr>
+          <tr>
+            <td>Positive percentage</td>
+            <td>{positivePercentage} %</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
@@ -22,35 +65,8 @@ const App = () => {
       <button onClick={handleNeutralClick}>Neutral</button>
       <button onClick={handleBadClick}>Bad</button>
       <h1>Statistics</h1>
-      <table>
-        <tbody>
-          <tr>
-            <td>Good</td>
-            <td>{good}</td>
-          </tr>
-          <tr>
-            <td>Neutral</td>
-            <td>{neutral}</td>
-          </tr>
-          <tr>
-            <td>Bad</td>
-            <td>{bad}</td>
-          </tr>
-          <tr>
-            <td>Total</td>
-            <td>{good + neutral + bad}</td>
-          </tr>
-          <tr>
-            <td>Average</td>
-            <td>{(good - bad) / (good + neutral + bad)}</td>
-          </tr>
-          <tr>
-            <td>Positive percentage</td>
 
-            <td>{(good / (good + neutral + bad)) * 100} %</td>
-          </tr>
-        </tbody>
-      </table>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </>
   );
 };
